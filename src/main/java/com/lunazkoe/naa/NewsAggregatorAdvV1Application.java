87a9de5548ce5626a1e266686ca1190f193b28e2
lunazkoe <lunazkoe@gmail.com>
2026-05-16 20:43:10 +0900

    first commit

---
+++ src/main/java/com/lunazkoe/naa/NewsAggregatorAdvV1Application.java
@@ -0,0 +1,13 @@
+package com.lunazkoe.naa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorAdvV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsAggregatorAdvV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lunazkoe/naa/NewsAggregatorAdvV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunazkoe.naa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorAdvV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
