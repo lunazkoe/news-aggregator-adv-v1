30d170e911ead6259a2fd18ff3022ab765dc87cb
lunazkoe <lunazkoe@gmail.com>
2026-05-16 21:24:24 +0900

    chore: SwaggerConfig.java 설정 완료 및 적용 Test

--- src/main/java/com/lunazkoe/naa/global/TestController.java
+++ src/main/java/com/lunazkoe/naa/global/TestController.java
@@ -1,4 +1,17 @@
 package com.lunazkoe.naa.global;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test-api")
 public class TestController {
+
+    @Operation(summary = "Swagger Config Test", description = "Swagger Config 적용 테스트")
+    @GetMapping
+    public String swaggerConfigTest() {
+        return "Swagger Config Test";
+    }
 }
