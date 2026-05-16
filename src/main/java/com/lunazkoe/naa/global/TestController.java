e77c277e12cb310d23f12d0adab2951eff3a082d
lunazkoe <lunazkoe@gmail.com>
2026-05-16 21:58:57 +0900

    chore: MDCLoggingFilter 및 logback-spring.xml 설정

--- src/main/java/com/lunazkoe/naa/global/TestController.java
+++ src/main/java/com/lunazkoe/naa/global/TestController.java
@@ -1,10 +1,12 @@
 package com.lunazkoe.naa.global;
 
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test-api")
 public class TestController {
@@ -12,6 +14,7 @@ public class TestController {
     @Operation(summary = "Swagger Config Test", description = "Swagger Config 적용 테스트")
     @GetMapping
     public String swaggerConfigTest() {
+        log.info("Swagger Config Test API Call");
         return "Swagger Config Test";
     }
 }
